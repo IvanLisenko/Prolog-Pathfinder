4943363fa8af58a4ba79334359bb675487e77b30
IvanLisenko <vanyalisenko1985@gmail.com>
2025-10-20 12:13:04 +0300

    update routes.pl

--- prolog/routes.pl
+++ prolog/routes.pl
@@ -1,4 +1,18 @@
 % road(City1, City2, Distance, Time)
-road(alushta, simferopol, 45, 50).
-road(simferopol, sevastopol, 80, 90).
-road(alushta, yalta, 35, 40).
+    road(simferopol, alushta, 45, 50).
+    road(simferopol, belogorsk, 44, 37).
+    road(simferopol, saki, 49, 37).
+    road(simferopol, oktyabrskoye, 44, 30).
+    road(simferopol, krasnoperekopsk, 124, 86).
+    road(simferopol, sevastopol, 78, 70).
+    road(alushta, yalta, 38, 29).
+    road(alishta, sudak, 81, 57).
+    road(belogorsk, feodosiya, 72, 75).
+    road(sudak, feodosiya, 53, 40).
+    road(belogorsk, primorskyi, 84, 92).
+    road(feodosiya, primorskyi, 14, 11).
+    road(primorskyi, kerch, 85, 68).
+    road(krasnoperekopsk, armyansk, 20, 15).
+    
+    
+
